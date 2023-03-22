5134ef6d6c7c55609ef92c28c6ef64abd8f4badb
Ngaaje-Ashu <ngaajeashu12@gmail.com>
2023-03-22 16:06:53 +0100

    Added some functionalities to the drum kit web

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 // document.querySelector("button").style.backgroundColor = "yellow";
 // document.querySelector("h1").classList.add("huge");
 // document.querySelector("a").setAttribute("href", "https://www.bing.com");
-
+//Detecting button clicked
 var numberOfDrumButtons = document.querySelectorAll(".drum").length;
 
 for (var i = 0; i < numberOfDrumButtons; i++) {
@@ -19,8 +19,8 @@ for (var i = 0; i < numberOfDrumButtons; i++) {
 
   });
 
-}
-
+}  
+//Detecting button pressed or key on keyboard pressed 
 document.addEventListener("keypress", function(event) {
 
   makeSound(event.key);
@@ -34,38 +34,38 @@ function makeSound(key) {
 
   switch (key) {
     case "w":
-      var tom1 = new Audio("sounds/kick-bass.mp3");
-      tom1.play();
+      var kickBass = new Audio("sounds/kick-bass.mp3");
+      kickBass.play();
       break;
 
     case "a":
-      var tom2 = new Audio("sounds/snare.mp3");
-      tom2.play();
-      break;
-
-    case "s":
-      var tom3 = new Audio('sounds/tom-1.mp3');
-      tom3.play();
-      break;
-
-    case "d":
-      var tom4 = new Audio('sounds/tom-2.mp3');
-      tom4.play();
-      break;
-
-    case "j":
-      var snare = new Audio('sounds/tom-3.mp3');
+      var snare = new Audio("sounds/snare.mp3");
       snare.play();
       break;
 
+    case "s":
+      var tom1 = new Audio('sounds/tom-1.mp3');
+      tom1.play();
+      break;
+
+    case "d":
+      var tom2 = new Audio('sounds/tom-2.mp3');
+      tom2.play();
+      break;
+
+    case "j":
+      var tom3 = new Audio('sounds/tom-3.mp3');
+      tom3.play();
+      break;
+
     case "k":
-      var crash = new Audio('sounds/tom-4.mp3');
-      crash.play();
+      var tom4 = new Audio('sounds/tom-4.mp3');
+      tom4.play();
       break;
 
     case "l":
-      var kick = new Audio('sounds/crash.mp3');
-      kick.play();
+      var crash = new Audio('sounds/crash.mp3');
+      crash.play();
       break;
 
 
